4d62544e28714696d44ec80715d1176876025ddd
skpixels <makkobir79@gmail.com>
2023-08-03 05:02:07 +0300

    fixed  the error of faq

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -14,7 +14,7 @@ function FAQPage() {
         <div className="flex-1">
           <div className="join join-vertical w-full">
             <div className="collapse collapse-arrow join-item border border-base-300">
-              <input type="radio" name="my-accordion-4" checked="checked" />
+              <input type="radio" name="my-accordion-4" />
               <div className="collapse-title text-xl font-medium">
                 Click to open this one and close others
               </div>
