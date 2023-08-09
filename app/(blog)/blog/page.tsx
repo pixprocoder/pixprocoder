b8928ce0ee6b23a932a3dde7a786d04be8bd9f39
skpixels <makkobir79@gmail.com>
2023-08-10 00:53:03 +0300

    blog section modified

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/vertical-logo.png";
 
 function BlogPage() {
-  return <section className="pt-8">This is blog page</section>;
+  return (
+    <section className="bg-gray-800 py-14">
+      <div className=" flex justify-around">
+        <Link href="/">
+          <Image src={logo} width={50} height={50} alt=" logo" />
+        </Link>
+        <p>Blog is coming soon</p>
+      </div>
+    </section>
+  );
 }
 
 export default BlogPage;
