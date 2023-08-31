c134d65c194fa0e4cf18d6615ee66954980bb75d
pixprocoder <makkobir79@gmail.com>
2023-08-31 12:14:24 +0300

    overview card commented and replit file deleted

--- app/(with-layout)/page.tsx
+++ app/(with-layout)/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <section className=" text-white">
       <Hero />
-      <OverviewCard />
+      {/* <OverviewCard /> */}
       <About />
       <Portfolio />
       <Skills />
