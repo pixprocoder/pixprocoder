483b8871ec83bcc16a4551c51d468b4cd8656c44
samsul kobir <makkobir79@gmail.com>
2023-10-29 02:22:50 +0300

    skill card modified

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -10,8 +10,8 @@ function Skills() {
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {technologies.map((t) => (
           <>
-            <div className="card hover:bg-blue-300 transition hover:border-none cursor-pointer duration-200 border-blue-500  border ">
-              <div className="card-body items-center text-center">
+            <div className="rounded hover:bg-blue-300 transition hover:border-none cursor-pointer duration-200 border-blue-500  border ">
+              <div className="flex gap-2 p-2 items-center text-center">
                 <Image width={50} height={50} src={t.icon} alt="Icons" />
                 <h2 className="card-title">{t.name}</h2>
               </div>
