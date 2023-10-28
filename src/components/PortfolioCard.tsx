e30b8f4f1011b909a64501eb162890b7877b1475
samsul kobir <makkobir79@gmail.com>
2023-10-29 01:44:10 +0300

    card replaced with shadcn card

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -4,6 +4,16 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 import Link from "next/link";
+import { cn } from "../lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Button } from "./ui/button";
 
 type Props = {
   id: string;
@@ -29,49 +39,31 @@ const PortfolioCard = ({
   }, []);
 
   return (
-    <div
+    <section
       data-aos="fade-up"
       data-aos-duration="500"
-      className="card flex flex-col justify-center items-center   shadow-xl hover:shadow-2xl "
+      className=" flex flex-col justify-center items-center   shadow-xl hover:shadow-2xl "
     >
-      <Link href={`/portfolio/${id}`}>
-        <div className=" mx-10 rounded-lg flex justify-center overflow-hidden">
-          <img
-            src={image}
-            alt="Image"
-            className="transform transition-transform duration-500 hover:scale-125"
-          />
-          {/* <p className="absolute bottom-0 text-blue-300  ">
-            Click Image to view case study
-          </p> */}
-        </div>
-      </Link>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <p>{description}</p>
-        <div className="flex gap-2">
-          {tags?.map((t: any, i: number) => (
-            <div key={i}>
-              <span className="text-green-600">#{t?.name}</span>
+      <Card className={cn("w-full")}>
+        <CardHeader>
+          <Link href={`/portfolio/${id}`}>
+            <div className="  rounded-lg flex justify-center overflow-hidden">
+              <img
+                src={image}
+                alt="Image"
+                className="transform transition-transform duration-500 hover:scale-125"
+              />
             </div>
-          ))}
-        </div>
-        <div className="card-actions justify-start">
-          <Link
-            className="p-2 underline border border-blue-500 rounded-md text-blue-500"
-            href={gitHubLink}
-          >
-            GitHub
           </Link>
-          <Link
-            className="p-2 border border-blue-500 underline text-blue-500 rounded-md"
-            href={liveLink}
-          >
-            Live Link
-          </Link>
-        </div>
-      </div>
-    </div>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
+
+        <CardFooter>
+          <Button className="w-full">Read Case Study</Button>
+        </CardFooter>
+      </Card>
+    </section>
   );
 };
 
