380b38a65cb6c5e7fa9ef3bb16078e75225a625b
samsul kobir <makkobir79@gmail.com>
2023-10-29 03:41:08 +0300

    loading page added

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+function LoadingPage() {
+  return (
+    <div className=" min-h-screen flex justify-center items-center ">
+      The page is loading...
+    </div>
+  );
+}
+
+export default LoadingPage;
