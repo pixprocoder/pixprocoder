f269c12d1742982cbf74443df00432c64b487636
samsul kobir <makkobir79@gmail.com>
2023-11-06 23:07:11 +0200

    dashboard left sidebar modified

--- src/app/(dashboard-layout)/dashboard/page.tsx
+++ src/app/(dashboard-layout)/dashboard/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/src/components/ui/button";
+import Link from "next/link";
+
 const DashboardPage = () => {
   return (
     <section className="max-w-[1280px] mx-auto">
@@ -6,19 +9,48 @@ const DashboardPage = () => {
         <div className="dashboard-card-bg  ">
           <div className="flex flex-col justify-between h-screen  p-2 text-sm">
             <div>
-              <h1>Kobir</h1>
+              <Button className="w-full">
+                <Link href="/">Kobir</Link>
+              </Button>
             </div>
             <div>
               <ul className="flex flex-col gap-4">
-                <li>HOME</li>
-                <li>INSTAGRAM</li>
-                <li>FACEBOOK</li>
-                <li>TWITTER</li>
-                <li>LINKEDIN</li>
+                <li>
+                  <Button className="w-full">
+                    <Link href="/">Home</Link>
+                  </Button>
+                </li>
+                <li>
+                  <Button className="w-full">
+                    <Link href="/">Blog</Link>
+                  </Button>
+                </li>
+                <li>
+                  <Button className="w-full">
+                    <Link href="/">Facebook</Link>
+                  </Button>
+                </li>
+                <li>
+                  <Button className="w-full">
+                    <Link href="/">Instagram</Link>
+                  </Button>
+                </li>
+                <li>
+                  <Button className="w-full">
+                    <Link href="/">Twitter</Link>
+                  </Button>
+                </li>
+                <li>
+                  <Button className="w-full">
+                    <Link href="/">LinkedIn</Link>
+                  </Button>
+                </li>
               </ul>
             </div>
             <div>
-              <h1>Logout</h1>
+              <Button className="w-full">
+                <Link href="/">Logout</Link>
+              </Button>
             </div>
           </div>
         </div>
