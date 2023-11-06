bd1d5d8095c6244637f9d568afa82c4b2fe76cf2
samsul kobir <makkobir79@gmail.com>
2023-11-06 22:38:34 +0200

    dashboard layout group added and css file modified

---
+++ src/app/(dashboard-layout)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+const DashboardPage = () => {
+  return (
+    <section className="max-w-[1280px] mx-auto">
+      <div className="dashboard-container gap-5">
+        <div className="dashboard-card-bg">Left</div>
+        <div className="dashboard-card-bg">Middle</div>
+        <div className="dashboard-card-bg">Right</div>
+      </div>
+    </section>
+  );
+};
+
+export default DashboardPage;
