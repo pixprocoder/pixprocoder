366aaece1cc201404fb535adc99ed00ebcf1d3b3
samsul kobir <makkobir79@gmail.com>
2024-01-23 17:08:03 +0200

    portfoil detail page edited

--- src/app/(with-layout)/portfolio/[id]/page.tsx
+++ src/app/(with-layout)/portfolio/[id]/page.tsx
@@ -8,26 +8,68 @@ function PortfolioDetailPage({ params }: any) {
 
   return (
     <div className="container mx-auto">
-      <h1 className="text-2xl my-2">{singleProject?.title}</h1>
-      <div className="w-2/4 mx-auto">
-        <Image width={640} height={640} src={singleProject?.image} alt="img" />
+      <h1 className="text-2xl my-2 text-center text-blue-500 font-bold ">
+        {singleProject?.title}
+      </h1>
+      <p className="text-xl my-4 underline ">Overview</p>
+      <p>{singleProject?.description}</p>
+      <div className="">
+        <p className="text-xl my-4 underline ">Screenshots</p>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+          <Image
+            width={640}
+            height={640}
+            src={singleProject?.image}
+            alt="img"
+          />
+          <Image
+            width={640}
+            height={640}
+            src={singleProject?.image}
+            alt="img"
+          />
+          <Image
+            width={640}
+            height={640}
+            src={singleProject?.image}
+            alt="img"
+          />
+          <Image
+            width={640}
+            height={640}
+            src={singleProject?.image}
+            alt="img"
+          />
+        </div>
       </div>
 
-      <div>
-        <p>{singleProject?.description}</p>
-      </div>
-      <div>
-        <p className="text-xl my-4 underline ">Features</p>
-        <ul className="my-4">
-          <li>👉 User can login</li>
-          <li>👉 User can Sign up</li>
-          <li>👉 User can Test</li>
-        </ul>
+      <div className="grid grid-col-2 lg:grid-cols-2">
+        <div>
+          <p className="text-xl my-4 underline ">Features</p>
+          <ul className="my-4">
+            <li>👉 User can login</li>
+            <li>👉 User can Sign up</li>
+            <li>👉 User can Test</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl my-4 underline ">Technologies</p>
+          <ul className="my-4">
+            <li>React.js</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+          </ul>
+        </div>
       </div>
       <div className="flex gap-3 my-4">
-        <Button href={singleProject?.gitHubLink} target="_blank">
-          <div className="flex gap-2 underline hover:text-blue-500">GitHub</div>
-        </Button>
+        <Link href={singleProject?.gitHubLink} target="_blank">
+          <Button>
+            <div className="flex gap-2 underline hover:text-blue-500">
+              GitHub
+            </div>
+          </Button>
+        </Link>
+
         <Link href={singleProject?.liveLink} target="_blank">
           <Button className="flex gap-2 underline hover:text-blue-500">
             Live Link
