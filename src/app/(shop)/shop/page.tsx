731ff12d583a72beddafc46c58f9df8beef97624
Samsul Kobir <pixprocoder@Samsuls-MacBook-Air.local>
2024-08-10 00:54:04 +0300

    confetii effect added

--- src/app/(shop)/shop/page.tsx
+++ src/app/(shop)/shop/page.tsx
@@ -1,7 +1,34 @@
-const Shop = () =>{
-    return(
-        <h1>Hello from shop page</h1>
-    )
-}
+"use client"
+import { Button } from "@/src/components/ui/button";
+import Confetti from 'react-confetti'
+import { useState } from "react";
+
+
+const Shop = () => {
+    const [isConfettiVisible, setConfettiVisible] = useState(false);
+
+    const handleClick = () => {
+      setConfettiVisible(true);
+      setTimeout(() => {
+        setConfettiVisible(false);
+      }, 4000); // Confetti lasts for 3 seconds
+    };
+  return (
+    
+    <div className="w-[1400px] mx-auto flex justify-center items-center min-h-screen">
+     
+      {/* <h1>Hello from shop page</h1> */}
+      <Button onClick={handleClick}>Click</Button>
+      {isConfettiVisible && (
+      <Confetti
+        width={window.innerWidth}
+        height={window.innerHeight}
+        numberOfPieces={200}
+        gravity={0.1}
+      />
+    )}
+    </div>
+  );
+};
 
 export default Shop;
