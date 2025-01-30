0149113864997e3f0d2a3786f8f5e87ce26ac65c
pixprocoder <makkobir79@gmail.com>
2025-01-30 23:59:07 +0200

    shipping address and item details done

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,8 +1,13 @@
+'use client';
 import { Button } from '@/src/components/ui/button';
+import { useAppSelector } from '@/src/redux/hooks/hooks';
 import Image from 'next/image';
 import Link from 'next/link';
+import { FaRegEdit } from 'react-icons/fa';
 
 function page() {
+  const { items } = useAppSelector((state) => state.cart);
+
   return (
     <div className="max-w-7xl mx-auto">
       <div className="flex mt-4 gap-4 items-center">
@@ -14,14 +19,81 @@ function page() {
       <hr className="border border-gray-600 my-4" />
       <div className="flex justify-between gap-4">
         <div className="flex-[2]">
+          {/* Shipping Address */}
           <div>
-            <h3 className="mb-2 text-gray-100">Shipping Address Details</h3>
-            <div>add shipping address form here</div>
+            <h3 className="mb-2 text-gray-300">Shipping Address </h3>
+            <div className="bg-gray-900 rounded-md  flex justify-between gap-3 px-2 py-4">
+              <div className="flex border-r border-gray-500 pr-2 flex-1 justify-between w-full">
+                <div>
+                  <p className="text-sm text-gray-300">
+                    Samsul Kobir
+                    <span className="pl-3">+33 5333456233</span>
+                  </p>
+                  <p className="text-xs my-1 text-purple-500">
+                    Calea Moșilor 237, block.45, sc.1, sector.2, ap.20, floor.3
+                    București, București 021243, Romania
+                  </p>
+                  <p className="text-sm text-gray-300">
+                    București, București 021243, Romania
+                  </p>
+                </div>
+                <FaRegEdit className="text-xl" />
+              </div>
+              <div className="flex-1">
+                <p className="text-sm text-gray-300">
+                  Delivery
+                  <span className="pl-3">FREE</span>
+                </p>
+                <p className="text-xs my-1 text-purple-500">
+                  Delivery: 4-12 business days, fastest delivery in 4 business
+                  days
+                </p>
+              </div>
+            </div>
+            <hr className="my-4 border-[0.1px] border-gray-700" />
           </div>
+          {/* Item Details */}
           <div>
-            <h3 className="mb-2 text-gray-100">Item Details goes here</h3>
-            <div>All Items here</div>
+            <div className="flex justify-between  ">
+              <h3 className="mb-2 text-sm text-gray-200">
+                <span className="text-purple-500">
+                  {items?.length && items?.length}
+                </span>{' '}
+                Items
+              </h3>{' '}
+              <span className="text-gray-300 text-sm underline cursor-pointer ">
+                View All
+              </span>
+            </div>
+
+            <div className="bg-gray-900 rounded-md flex justify-between  px-2 py-4">
+              <div className="flex gap-4 overflow-auto scrollbar-thin scrollbar-thumb-purple-500 scrollbar-track-gray-700 py-3">
+                {items?.map((item: any) => {
+                  return (
+                    <div
+                      key={item.id}
+                      className="flex flex-col cursor-pointer  "
+                    >
+                      <div className="w-[100px] overflow-hidden border-gray-800 border-[0.1px] rounded-md h-[100px] flex justify-center items-center">
+                        <Image
+                          src={item?.image}
+                          width={100}
+                          height={100}
+                          alt="product"
+                        />
+                      </div>
+                      <h1 className=" text-xl text-purple-500 font-bold mt-2">
+                        $ {item?.price}
+                      </h1>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
+          <hr className="my-4  border-[0.1px]  border-gray-700" />
+
+          {/* payment  */}
           <div>
             <h3 className="mb-2 text-gray-100">Payment Method</h3>
             <div>Payment Method here</div>
