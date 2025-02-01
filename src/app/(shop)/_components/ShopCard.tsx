e1eff9afb2cdbe0e38b365528934a68f39cf0403
pixprocoder <makkobir79@gmail.com>
2025-02-01 16:56:06 +0200

    rating and count added dynamically

--- src/app/(shop)/_components/ShopCard.tsx
+++ src/app/(shop)/_components/ShopCard.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
-import { FaRegStar } from 'react-icons/fa';
+import { FaRegStar, FaStarHalfAlt } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
 
 const ShopCard = ({ item }: any) => {
+  console.log(item);
   return (
     <Link
       href={`/shop/${item.id}`}
@@ -17,14 +19,45 @@ const ShopCard = ({ item }: any) => {
         </p>
         <div className="flex flex-row-reverse justify-between my-2">
           <span className="text-xs text-gray-500">In Stock</span>
-          <span className="text-sm text-gray-600 flex gap-1 items-center">
+          <div className="text-sm text-gray-600 flex gap-1 items-center">
+            {[...Array(5)].map((_, index) => {
+              if (index < Math.floor(item?.rating?.rate)) {
+                // Full star
+                return (
+                  <span key={index} className="text-yellow-400">
+                    <FaStar />
+                  </span>
+                );
+              } else if (index < item?.rating?.rate) {
+                // Half star
+                return (
+                  <span key={index} className="text-yellow-400">
+                    <FaStarHalfAlt />
+                  </span>
+                );
+              } else {
+                // Empty star
+                return (
+                  <span key={index} className="text-gray-400">
+                    <FaRegStar />
+                  </span>
+                );
+              }
+            })}
+            <span className="text-xs text-purple-500">
+              ({item?.rating?.count})
+            </span>
+          </div>
+          {/* <div className="text-sm text-gray-600 flex gap-1 items-center">
             <FaRegStar />
             <FaRegStar />
             <FaRegStar />
             <FaRegStar />
             <FaRegStar />
-            <span className="text-xs">(0)</span>
-          </span>
+            <span className="text-xs text-purple-500">
+              ({item?.rating?.count})
+            </span>
+          </div> */}
         </div>
         <span className="text-purple-500 font-black text-2xl">
           ${item.price}
